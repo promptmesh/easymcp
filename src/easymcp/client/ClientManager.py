66d26272f44b733f604cb3049a305d02afa7b44c
evalstate <1936278+evalstate@users.noreply.github.com>
2025-03-15 17:42:20 +0000

    adjust scheme to have base url

--- src/easymcp/client/ClientManager.py
+++ src/easymcp/client/ClientManager.py
@@ -130,7 +130,7 @@ class ClientManager:
                     "http",
                     "https",
                 ):
-                    resource.uri = AnyUrl(f"mcp-{name}+{resource.uri}")
+                    resource.uri = AnyUrl(f"mcp://{name}+{resource.uri}")
 
                 result.append(resource)
 
