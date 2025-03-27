f3706ea1c81f3fdaf3f167a99ba190f42db19cf6
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-03-27 21:48:28 +0000

    add sse transport

--- src/easymcp/client/transports/sse.py
+++ src/easymcp/client/transports/sse.py
@@ -1,7 +1,8 @@
 import httpx
 from pydantic import BaseModel
 from easymcp.client.transports.generic import TransportProtocol
-
+from mcp.client.sse import sse_client
+from mcp import types
 
 class SseServerParameters(BaseModel):
     """SseServerParameters class"""
@@ -12,13 +13,18 @@ class SseServerParameters(BaseModel):
     headers: dict[str, str] = {}
     """headers to send"""
 
+    timeout: int = 60
+    """timeout in seconds"""
+
 
 class SseTransport(TransportProtocol):
     """SseTransport class"""
 
     args: SseServerParameters
-    client: httpx.AsyncClient
     connection = None
+
+    readsream = None
+    writestream = None
 
     def __init__(self, arguments: SseServerParameters):
         self.state = "constructed"
@@ -27,22 +33,33 @@ class SseTransport(TransportProtocol):
     async def init(self):
         """Perform init logic"""
         self.state = "initialized"
-
-        raise NotImplementedError
+        self.client = sse_client(self.args.url, headers=self.args.headers, timeout=self.args.timeout)
 
     async def start(self):
         """Start the transport"""
         self.state = "started"
 
-        raise NotImplementedError
+        self.readsream, self.writestream = await self.client.__aenter__()
+
 
     async def send(self, message: str):
         """Send data to the transport"""
-        raise NotImplementedError
+        assert self.writestream, "Transport not started"
+
+        msg = types.JSONRPCMessage.model_validate_json(message)
+        await self.writestream.send(msg)
 
     async def receive(self) -> str:
         """Receive data from the transport"""
-        raise NotImplementedError
+        assert self.readsream, "Transport not started"
+
+        msg = await self.readsream.receive()
+        
+        if isinstance(msg, Exception):
+            await self.stop()
+            raise msg
+        
+        return msg.model_dump_json()
 
     async def stop(self):
         """Stop the transport"""
