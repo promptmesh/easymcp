534ebca0384bb46c9762e56a8e64b3940cbfc57d
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-04-04 19:46:37 +0100

    test prompts

--- tests/test_stdio_session_prompts.py
+++ tests/test_stdio_session_prompts.py
@@ -1,9 +1,11 @@
 import asyncio
+from mcp import GetPromptResult, InitializeResult, ListPromptsResult
+import pytest
 from easymcp.client.sessions.mcp import MCPClientSession
 from easymcp.client.transports.stdio import StdioTransport, StdioServerParameters
 
-
-async def main():
+@pytest.mark.asyncio()
+async def test_stdio_client_session_prompts():
     args = StdioServerParameters(command="uvx", args=["mcp-wolfram-alpha"], env={"WOLFRAM_API_KEY": "DEMO"})
     transport = StdioTransport(args)
 
@@ -11,18 +13,13 @@ async def main():
     await client_session.init()
 
     result = await client_session.start()
-    print(f"{result=}")
-
-    # await asyncio.sleep(10)
+    assert isinstance(result, InitializeResult)
 
     prompts = await client_session.list_prompts()
-    print(f"{prompts=}")
+    assert isinstance(prompts, ListPromptsResult)
 
     resolved = await client_session.read_prompt("wa", {"query": "pi"})
-    print(f"{resolved=}")
+    assert isinstance(resolved, GetPromptResult)
 
-    # await client_session.stop()
-    await asyncio.Future()
-
-
-asyncio.run(main())
+    await client_session.stop()
+    await asyncio.sleep(0)
